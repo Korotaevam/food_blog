28432d6fc37157dd765ff90e9f2efee883d37f7b
Korotaevam <kam888@mail.ru>
2023-04-11 16:04:05 +0300

    add urls index

--- food_blog/food_blog/settings.py
+++ food_blog/food_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*&=lqe+_s!gjj1k0@)(s%qnosj_p2-sho&36gmzn94dlxe!pi+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'recipes.apps.RecipesConfig',
+
+
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'food_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,9 +119,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
--- food_blog/recipes/views.py
+++ food_blog/recipes/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+from recipes.models import Recipe
+
+
+def index(request):
+    posts = Recipe.objects.all()
+    return render(request, 'recipes/index.html', {'posts': posts})
+
+
+def about(request):
+    return render(request, 'recipes/about.html')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Page not Found</h1>')
